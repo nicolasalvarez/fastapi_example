abf7f256f684464919f955bdc89125cdc33ea45d
nalvarez <nicoalar.22@gmail.com>
2024-02-05 12:39:30 -0300

    more about parameters

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from enum import Enum
+from pydantic import BaseModel, Field, HttpUrl
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import Body, FastAPI, Query, Path
 
 
 app = FastAPI()
@@ -17,26 +17,26 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 
 # This should be called as "http://127.0.0.1:8000/items/?skip=0&limit=10"
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
 
 
 # optional_param param is optional
 # needy, a required str.
 # skip, an int with a default value of 0.
 # limit, an optional int.
-@app.get("/items/{item_id}")
-async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: int | None = None
-):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+# @app.get("/items/{item_id}")
+# async def read_user_item(
+#     item_id: str, needy: str, skip: int = 0, limit: int | None = None
+# ):
+#     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+#     return item
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
 
 
 # /users/me must be declared before the one for /users/{user_id} Otherwise,
@@ -91,13 +91,33 @@ async def read_user_item(
 
 
 # Request Body
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 class Item(BaseModel):
     name: str
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: float | None = None
+    tags: set[str] = set()
+    images: list[Image] | None = None
+
+class Offer(BaseModel):
+    name: str
     description: str | None = None
     price: float
-    tax: float | None = None
+    items: list[Item]
 
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
 
 @app.post("/items/")
 async def create_item(item: Item):
@@ -108,15 +128,74 @@ async def create_item(item: Item):
     return item_dict
 
 
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+    item: Item, 
+    user: User, 
+    importance: Annotated[int, Body(gt=0)],
+):
+    """
+        Expected Body:
+        {
+            "item": {
+                "name": "Foo",
+                "description": "The pretender",
+                "price": 42.0,
+                "tax": 3.2
+            },
+            "user": {
+                "username": "dave",
+                "full_name": "Dave Grohl"
+            },
+            "importance": 5
+        }
+    """
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
 # Request body + path + query parameters
 # * If the parameter is also declared in the path, it will be used as a path parameter.
 # * If the parameter is of a singular type (like int, float, str, bool, etc)
 #   it will be interpreted as a query parameter.
 # * If the parameter is declared to be of the type of a Pydantic model,
 #   it will be interpreted as a request body.
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
+    size: Annotated[float, Query(gt=0, lt=10.5)],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
+):
+    results = {"item_id": item_id}
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    results.update({"size": size})
+    return results
+
+
+# http://127.0.0.1:8000/items/?no-pythonic-name=fixedquery&r=required_param&s=some1&s=some2
+@app.get("/items/")
+async def read_items(    
+    r: Annotated[
+        str, Query(min_length=3, max_length=50, pattern="^required_param$")
+    ],
+    p: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+    ] = "default_value",
+    q: Annotated[
+        str | None, Query(title="Query string",
+                          description="Query string for the items to search in the database that have a good match",
+                          alias="no-pythonic-name",
+                          min_length=3, max_length=50, pattern="^fixedquery$")
+    ] = None,
+    s: Annotated[list[str], Query()] = ["optional_1", "optional_2"],
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if p:
+        results.update({"p": p})
+    if q:
+        results.update({"q": q})
+    results.update({"r": r})
+    results.update({"s": s})
+
+    return results
